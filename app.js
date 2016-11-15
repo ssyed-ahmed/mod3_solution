40b9c9858dc3a3c0a786c97dd7e1a35eb013710d
Sumair <sumair@coursera.com>
2016-11-15 23:27:18 +1100

    modified files.

--- app.js
+++ app.js
@@ -5,11 +5,25 @@
     .controller('NarrowItDownController', NarrowItDownController)
     .service('MenuSearchService', MenuSearchService);
 
-  NarrowItDownController.$inject = ['MenuSearchService'];
-  function NarrowItDownController(MenuSearchService) {
+  NarrowItDownController.$inject = ['MenuSearchService', '$q'];
+  function NarrowItDownController(MenuSearchService, $q) {
       var search = this;
 
-      search.searchTerm = "";
+      search.searchTerm;
+
+      search.getMatchedMenuItems = function(searchTerm) {
+        var deferred = $q.defer();
+        var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+        promise.then(function(result) {
+          var items = result;
+          $q.resolve(items);
+          return items;
+        },
+        function(error) {
+          console.log("Error while fetching menu items.", error);
+        });
+        return deferred.promise;
+      }
 
   }
 
@@ -18,26 +32,23 @@
     var service = this;
 
     service.getMatchedMenuItems = function(searchTerm) {
-
-        var promise = $http({
+        var foundItems = [];
+        return $http({
           method: 'GET',
           url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
-        });
-
-        promise.then(function(result) {
-            var items = result.data;
+        }).then(function(result) {
+            var items = result.data.menu_items;
             for (var i = 0; i < items.length; i++) {
               var item = items[i];
               if (item.description.toLowerCase().indexOf(searchTerm) !== -1) {
-                return true;
+                foundItems.push(item);
               }
             }
-            return false;
-        })
-        .catch(error) {
+            return foundItems;
+        },
+        function(error) {
           console.log("Error while fetching menu items.", error);
-        }
-        return promise;
+        });
     };
   }
 
